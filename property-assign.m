b9e8eb2f20167a36f852448c997fd817280ecab4
Zach Spoelstra <zspoelstra@gmail.com>
2013-12-19 09:48:35 -0500

    Added an assign property shortcut

---
+++ property-assign.m
@@ -0,0 +1,9 @@
+// Assign property
+// Creates an assign, nonatomic property
+//
+// Platform: All
+// Language: Objective-C
+// Completion Shortcut: propa
+// Completion Scope: Class Interface Methods
+
+@property (assign, nonatomic) <#type#> <#name#>;
