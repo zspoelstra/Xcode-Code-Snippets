e31c96d6eecb8fa30cbda2a54b4b8f20bb32a2ca
Zach Spoelstra <zspoelstra@gmail.com>
2014-01-30 10:23:48 -0500

    Add strong and weak self snippets
    See https://github.com/mattt/Xcode-Snippets

---
+++ stongself.m
@@ -0,0 +1,9 @@
+// __strong self
+// Declare strong reference to weak reference
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Shortcut: strongself
+// Completion Scope: Function or Method
+
+__strong __typeof(<#weakSelf#>)strongSelf = <#weakSelf#>;
---
+++ weakself.m
@@ -0,0 +1,9 @@
+// __weak self
+// Declare weak reference to self
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Shortcut: weakself
+// Completion Scope: Function or Method
+
+__weak typeof(self)weakSelf = self;
